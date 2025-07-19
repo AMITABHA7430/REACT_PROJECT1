8bec423bea14ea6fe5bec872324ffa1762ac66c1
AMITABHA7430 <amitabhag17@gmail.com>
2025-07-19 23:23:17 +0530

    Add files via upload

---
+++ custom-react/customreact.js
@@ -0,0 +1,22 @@
+function customRender(reactElement, mainContainer) {
+    const domElement = document.createElement(reactElement.type);
+    
+    domElement.innerHTML = reactElement.children;
+
+    domElement.setAttribute('href', reactElement.props.href);
+    domElement.setAttribute('target', reactElement.props.target);
+
+    mainContainer.appendChild(domElement);
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com/',
+        target: '_blank'
+    },
+    children: 'click me to visit google'  // ❗ Fixed typo here
+};
+
+const mainContainer = document.querySelector('#root');
+customRender(reactElement, mainContainer);
